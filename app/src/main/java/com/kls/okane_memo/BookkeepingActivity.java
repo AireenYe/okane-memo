e72598facafd4cad7752a9c1b73f3b2e5d6d5a7d
LeoLmy <leolmy@qq.com>
2021-09-16 21:39:10 +0800

    按照设计稿进行第一次更改

--- app/src/main/java/com/kls/okane_memo/BookkeepingActivity.java
+++ app/src/main/java/com/kls/okane_memo/BookkeepingActivity.java
@@ -30,49 +30,6 @@ public class BookkeepingActivity extends AppCompatActivity {
 
         backIv = findViewById(R.id.record_iv_back);
         backIv.setOnClickListener(new OnClick());
-
-        initPages();
-
-    }
-
-    private void initPages(){
-        tabLayout = findViewById(R.id.record_tabs);
-        viewPager = findViewById(R.id.record_vp);
-
-        List<Fragment> fragmentList = new ArrayList<>();
-        RecordFragment f1 = new RecordFragment();
-        RecordFragment f2 = new RecordFragment();
-        fragmentList.add(f1);
-        fragmentList.add(f2);
-        RecordAdapter recordAdapter = new RecordAdapter(getSupportFragmentManager(), getLifecycle(), fragmentList);
-        viewPager.setAdapter(recordAdapter);
-        ArrayList<String> title = new ArrayList<>();
-        title.add("支出");
-        title.add("收入");
-
-        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText(title.get(position));
-            }
-        }).attach();
-
-//        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                super.onPageSelected(position);
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//                super.onPageScrollStateChanged(state);
-//            }
-//        });
     }
 
     private class OnClick implements View.OnClickListener{
