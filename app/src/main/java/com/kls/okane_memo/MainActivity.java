2ad4db6b377cda76333b33bd7904c3623fc90b6f
Sienna-Xu <xuyuezzg@126.com>
2021-09-18 15:18:12 +0800

    "将datePicker加到主界面中"

--- app/src/main/java/com/kls/okane_memo/MainActivity.java
+++ app/src/main/java/com/kls/okane_memo/MainActivity.java
@@ -2,16 +2,26 @@ package com.kls.okane_memo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity{
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener,
+        DatePickerDialog.OnDateSetListener{
 
     public FloatingActionButton recordBtn;
+    public TextView dateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +29,40 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         recordBtn = findViewById(R.id.btn_record);
-        recordBtn.setOnClickListener(new OnClick());
+        recordBtn.setOnClickListener(this);
 
+        dateTextView = findViewById(R.id.datePicker);
+        dateTextView.setOnClickListener(this);
+        Calendar calendar = Calendar.getInstance();
+        String todayDate = String.format("%d月%d日",calendar.get(Calendar.MONTH)+1,calendar.get(Calendar.DAY_OF_MONTH));
+        dateTextView.setText(todayDate);
     }
 
-    private class OnClick implements View.OnClickListener{
-        @Override
-        public void onClick(View view) {
-            Intent intent = null;
-            switch (view.getId()) {
-                case R.id.btn_record:
-                    intent = new Intent(MainActivity.this, BookkeepingActivity.class);
-                    break;
-            }
-            startActivity(intent);
-            overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+    @Override
+    public void onDateSet(DatePicker datePicker,int year, int month, int dayOfMonth) {
+        String date=String.format("%d月%d日",month+1,dayOfMonth);
+        dateTextView.setText(date);
+        Log.d("日期",date);
+    }
 
+    @Override
+    public void onClick(View view){
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btn_record:
+                intent = new Intent(MainActivity.this, BookkeepingActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                break;
+            case R.id.datePicker:
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog dialog = new DatePickerDialog(this, this,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                dialog.getDatePicker().setMaxDate(calendar.getTimeInMillis());
+                dialog.show();
+                break;
         }
     }
 
